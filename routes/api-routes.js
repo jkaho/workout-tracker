6358f20392bfbaad3bcd805343c5b5da4a609710
jkaho <72959522+jkaho@users.noreply.github.com>
2021-03-30 17:22:53 +1100

    Add api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,17 +1,49 @@
+const Workout = require("../models/Workout");
+
 module.exports = (app) => {
-    app.get("/api/workouts", (data, res) => {
+    app.get("/api/workouts", res => {
         // Get all workouts
+        Workout.find({})
+            .then(allWorkouts => {
+                res.json(allWorkouts);
+            })
+            .catch(err => {
+                res.json(err);
+            });
     });
 
     app.post("/api/workouts", (data, res) => {
         // Create workout 
+        const workout = new Workout(data);
+
+        Workout.create(workout)
+            .then(newWorkout => {
+                res.json(newWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
     });
 
-    app.put("/api/workouts/:id", (data, res) => {
+    app.put("/api/workouts/:id", (req, res) => {
         // Add exercise to workout
+        Workout.findByIdAndUpdate(req.params.id, req.body)
+            .then(updatedWorkout => {
+                res.json(updatedWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
     });
 
     app.get("/api/workouts/range", (data, res) => {
         // Get all workouts (?)
+        Workout.find({})
+            .then(allWorkouts => {
+                res.json(allWorkouts);
+            })
+            .catch(err => {
+                res.json(err);
+            });
     })
 }
