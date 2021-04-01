c99129cfd077bef8e1fea920837fccc22ccf2f3c
jkaho <72959522+jkaho@users.noreply.github.com>
2021-04-01 15:35:47 +1100

    Render all workouts to workout history page

--- public/js/history.js
+++ public/js/history.js
@@ -1,8 +1,48 @@
 function showWorkoutHistory(workoutHistory) {
+    const contentDiv = document.querySelector(".history-content");
+
+    // Loop through each workout
     workoutHistory.forEach(workout => {
         const workoutDiv = document.createElement("div");
         workoutDiv.classList.add("workout-div");
-        
+
+        // Loop through each workout exercise
+        const workoutExercises = workout.exercises;
+        workoutExercises.forEach(exercise => {
+            const exerciseDiv = document.createElement("div");
+            exerciseDiv.classList.add("exercise-div");
+            
+            const exerciseNameDiv = document.createElement("div");
+            exerciseNameDiv.textContent = exercise.name;
+            const exerciseTypeDiv = document.createElement("div");
+            exerciseTypeDiv.textContent = exercise.type;
+            const exerciseDurationDiv = document.createElement("div");
+            exerciseDurationDiv.textContent = exercise.duration;
+
+            workoutDiv.appendChild(exerciseNameDiv);
+            workoutDiv.appendChild(exerciseTypeDiv);
+            workoutDiv.appendChild(exerciseDurationDiv);
+
+            if (exercise.type === "cardio") {
+                const exerciseDistanceDiv = document.createElement("div");
+                exerciseDistanceDiv.textContent = exercise.distance;
+
+                workoutDiv.appendChild(exerciseDistanceDiv);
+            } else {
+                const exerciseWeightDiv = document.createElement("div");
+                exerciseWeightDiv.textContent = exercise.weight;
+                const exerciseSetsDiv = document.createElement("div");
+                exerciseSetsDiv.textContent = exercise.sets;
+                const exerciseRepsDiv = document.createElement("div");
+                exerciseRepsDiv.textContent = exercise.reps;
+
+                workoutDiv.appendChild(exerciseWeightDiv);
+                workoutDiv.appendChild(exerciseSetsDiv);
+                workoutDiv.appendChild(exerciseRepsDiv);
+            }
+        })
+
+        contentDiv.appendChild(workoutDiv);
     })
 }
 
