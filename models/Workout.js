5022f7eb568bc2257a1f0e8238c6546c66a9bb12
jkaho <72959522+jkaho@users.noreply.github.com>
2021-04-04 02:17:50 +1100

    Fix 'Add workout' link on history page when there are no workouts

--- models/Workout.js
+++ models/Workout.js
@@ -6,7 +6,7 @@ const WorkoutSchema = new Schema(
     {
         day: {
             type: Date,
-            default: Date.now()
+            default: Date.now
         },
         exercises: [
             {
